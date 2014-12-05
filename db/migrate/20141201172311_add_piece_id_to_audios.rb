9800a1f2be0a1821421fdb2c94106b23a2dda699
smith freeman <smithifreeman@gmail.com>
2014-12-05 13:02:20 -0800

    heroku 2

--- db/migrate/20141201172311_add_piece_id_to_audios.rb
+++
@@ -1,6 +0,0 @@
-class AddPieceIdToAudios < ActiveRecord::Migration
-  def change
-    add_column :audios, :piece_id, :integer
-    change_column :audios, :piece_id, :integer, null: false
-  end
-end
